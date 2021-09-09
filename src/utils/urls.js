a7a5fab8dbdae3a05357e1a695090d610726b7b4
Wallison Silva <wallison.silva@meta.com.br>
2021-09-09 14:10:08 -0300

    feat(utils): add urls

---
+++ src/utils/urls.js
@@ -0,0 +1,8 @@
+export const BASE_URL = '/';
+export const SIGNIN_URL = '/signin';
+export const SIGNUP_URL = '/signup';
+export const SIGNIN_BG_URL = 'https://source.unsplash.com/random';
+export const API_URL = 'https://fq0as3lt8o.api.quickmocker.com';
+export const USERS_API_URL = `${API_URL}/users`;
+export const PURCHASES_API_URL = `${API_URL}/purchases`;
+export const NEW_USER_API_URL = `${API_URL}/new-user`;
